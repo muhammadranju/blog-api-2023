f0cfec40da8308431fa6a369dd36a14ebb23dc84
Muhāmmad Ranju <muhamadranju@gmail.com>
2023-10-30 22:59:45 +0600

    index change

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ const server = http.createServer(app);
 const start = async () => {
   try {
     await connectDB();
-    server.listen(PORT, () => {
-      console.log(`Server is running at http://localhost:${PORT}`);
-    });
   } catch (e) {
     console.log("Database Error");
     console.log(e);
@@ -19,3 +16,6 @@ const start = async () => {
 };
 
 start();
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
