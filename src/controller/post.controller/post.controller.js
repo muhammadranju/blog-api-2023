2c83592b415e9b58a396495d6a6a7ee1e526d6a5
Muhāmmad Ranju <muhamadranju@gmail.com>
2023-11-07 01:00:27 +0600

    post controller update

--- src/controller/post.controller/post.controller.js
+++ src/controller/post.controller/post.controller.js
@@ -1,17 +1,14 @@
 const { validationResult } = require("express-validator");
-const NodeCache = require("node-cache");
 const shortId = require("shortid");
 
 const Post = require("../../libs/post.libs/post.libs");
 const Comment = require("../../libs/comment.libs/comment.libs");
 
+const constants = require("../../config/constants");
+const ApiResponse = require("../../utils/ApiResponse");
+const asyncHandler = require("../../utils/asyncHandler");
 const errorFormatter = require("../../utils/errorFormatter/errorFormatter");
 const lowercaseText = require("../../utils/lowercaseText.utils/lowercaseText.utils");
-const response = require("../../utils/response.utils/response.utils");
-const constants = require("../../config/constants");
-
-const defaults = require("../../config/defaults");
-const myCache = new NodeCache({ stdTTL: 60 });
 
 const myUtils = (title) => {
   const shortId3 = shortId().slice(0, 3);
@@ -23,116 +20,101 @@ const myUtils = (title) => {
 };
 /*========================================================================================== */
 
-const getArticlesController = async (req, res, next) => {
+const getArticlesController = asyncHandler(async (req, res, next) => {
   const { limit, page, search } = req.query;
 
-  try {
-    const posts = await Post.findAllPosts({
+  const posts = await Post.findAllPosts({
+    page,
+    limit,
+    sort: { updatedAt: -1 },
+    search,
+  });
+
+  if (posts.length === 0) {
+    throw new ApiResponse(404, "fail", "The post was not found");
+  }
+
+  return res.status(200).json({
+    data: posts,
+    pagination: {
       page,
       limit,
-      sort: { updatedAt: -1 },
-      search,
-    });
+      next: 3,
+      prev: 1,
+      totalPage: 5,
+      totalItems: 50,
+    },
+    links: {
+      self: `/articles?page=2&limit=10`,
+      next: "/articles?page=3&limit=10",
+      prev: "/articles?page=1&limit=10",
+    },
+  });
+});
 
-    if (posts.length === 0) {
-      return response(res, "The post was not found", 404);
-    }
-
-    return res.status(200).json({
-      data: posts,
-      pagination: {
-        page,
-        limit,
-        next: 3,
-        prev: 1,
-        totalPage: 5,
-        totalItems: 50,
-      },
-      links: {
-        self: `/articles?page=2&limit=10`,
-        next: "/articles?page=3&limit=10",
-        prev: "/articles?page=1&limit=10",
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-const postArticleController = async (req, res, next) => {
+const postArticleController = asyncHandler(async (req, res, next) => {
   const errors = validationResult(req).formatWith(errorFormatter);
   if (!errors.isEmpty()) {
     return res.status(400).json({ error: errors.mapped() });
   }
-  try {
-    let { title, bodyText, cover, tags } = req.body;
 
-    const title_url = myUtils(title);
+  let { title, bodyText, cover, tags } = req.body;
 
-    tags = lowercaseText(tags, " ");
+  const title_url = myUtils(title);
 
-    const post = await Post.createPost({
-      title,
-      title_url,
-      bodyText,
-      cover,
-      tags,
-      author: req.user._id,
-    });
+  tags = lowercaseText(tags, " ");
 
-    // await post.save();
+  const post = await Post.createPost({
+    title,
+    title_url,
+    bodyText,
+    cover,
+    tags,
+    author: req.user._id,
+  });
 
-    return res.status(201).json({
-      code: 201,
-      message: "Article created successfully",
-      data: post,
-      links: {
-        self: `/articles/${post.title_url}`,
-        author: `/articles/${req.user.username}/author`,
-        comments: `/articles/1/comments`,
-      },
-    });
-  } catch (error) {
-    console.log(error.message);
-    next(error);
+  // await post.save();
+
+  return res.status(201).json({
+    code: 201,
+    message: "Article created successfully",
+    data: post,
+    links: {
+      self: `/articles/${post.title_url}`,
+      author: `/articles/${req.user.username}/author`,
+      comments: `/articles/1/comments`,
+    },
+  });
+});
+
+const getSingleArticleController = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const findOnePost = await Post.findSinglePost({ id });
+  if (!findOnePost) {
+    throw new ApiResponse(404, "fail", "This post not was found.");
   }
-};
+  const comment = await Comment.findCommentIsApproved(
+    { post: findOnePost.id },
+    { status: constants.status.approved }
+  );
+  console.log(comment);
 
-const getSingleArticleController = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const findOnePost = await Post.findSinglePost({ id });
-    if (!findOnePost) {
-      return response(res, "This post was not found", 400);
-    }
-    const comment = await Comment.findCommentIsApproved(
-      { post: findOnePost.id },
-      { status: constants.status.approved }
-    );
-    console.log(comment);
+  return res.status(200).json({
+    data: findOnePost,
+    comments: comment,
+    links: {
+      self: `/api/v1/articles/${id}`,
+      author: `/articles/${id}/${findOnePost.author.username}`,
+      comments: `/articles/${id}/comments`,
+    },
+  });
+});
+const putSingleArticlesUpdateController = asyncHandler(
+  async (req, res, next) => {}
+);
 
-    return res.status(200).json({
-      data: findOnePost,
-      comments: comment,
-      links: {
-        self: `/api/v1/articles/${id}`,
-        author: `/articles/${id}/${findOnePost.author.username}`,
-        comments: `/articles/${id}/comments`,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-const putSingleArticlesUpdateController = async (req, res, next) => {
-  try {
-  } catch (error) {
-    next(error);
-  }
-};
-
-const patchSingleArticleUpdateController = async (req, res, next) => {
-  try {
+const patchSingleArticleUpdateController = asyncHandler(
+  async (req, res, next) => {
     const id = req.params.id;
     let { title, bodyText, cover, tags, status } = req.body;
     title ?? title;
@@ -154,19 +136,18 @@ const patchSingleArticleUpdateController = async (req, res, next) => {
       tags,
       status
     );
+
     console.log(post);
     return res.status(201).json(post);
-  } catch (error) {
-    next(error);
   }
-};
+);
 
-const deleteSingleArticlesDeleteController = async (req, res, next) => {
-  try {
+const deleteSingleArticlesDeleteController = asyncHandler(
+  async (req, res, next) => {
     const id = req.params.id;
     const postDelete = await Post.findPostAndDelete({ id });
     if (!postDelete) {
-      return response(res, "Post not available", 400);
+      throw new ApiResponse(404, "fail", "Post not available", 404);
     }
     console.log("Article deleted successfully");
     return res.status(204).json({
@@ -174,10 +155,8 @@ const deleteSingleArticlesDeleteController = async (req, res, next) => {
       status: 204,
       message: "Article deleted successfully",
     });
-  } catch (error) {
-    next(error);
   }
-};
+);
 
 module.exports = {
   getArticlesController,
