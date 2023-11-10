7063b844057df1620fac06b61b75c3a0c48ab903
Muhāmmad Ranju <muhamadranju@gmail.com>
2023-11-10 23:34:36 +0600

    authentication function update

--- src/middleware/authentication.middleware/authentication.middleware.js
+++ src/middleware/authentication.middleware/authentication.middleware.js
@@ -1,28 +1,33 @@
 const User = require("../../libs/user.libs/user.libs");
 
-const jwt = require("../../service/jwtGenerator.service/jwtGenerator.service");
+const jwt = require("jsonwebtoken");
 const response = require("../../utils/response.utils/response.utils");
+
+function jwtVerifyToken(token) {
+  return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+}
 
 async function authentication(req, res, next) {
   try {
     //TODO: let token = req.headers.authorization || req.cookies.access_token;
     let token = req.headers.authorization;
     if (token === undefined) {
-      return response(res, "Unauthorized access", 401);
+      return response(res, "Unauthorized invalid access", 401);
     }
     token = token.split(" ")[1];
 
-    const { user_id } = jwt.jwtVerifyToken(token);
+    const { user_id } = jwtVerifyToken(token);
 
     if (!user_id) {
-      return response(res, "Unauthorized token", 401);
+      return response(res, "Unauthorized invalid token", 401);
     }
     req.user = await User.findUserById(user_id);
 
     return next();
   } catch (error) {
     console.log(error.message);
-    return response(res, "Unauthorized token", 401);
+    return response(res, "Unauthorized invalid token", 401);
+    return next(error);
   }
 }
 module.exports = authentication;
